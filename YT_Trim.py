2c613f3c4a42e9b04dbd2c0eab10a2cee7e7518f
Ancen0 <adamkosek774@gmail.com>
2023-06-02 12:20:54 +0200

    v1.0.2
    Added UserInput class
    Removed repeated logic

--- YT_Trim.py
+++ YT_Trim.py
@@ -2,12 +2,43 @@ from pytube import YouTube
 from pytube.exceptions import RegexMatchError
 from moviepy.editor import VideoFileClip
 import os
+import sys
 
-user = os.getlogin()
+class UserInput:
+    def __init__(self):
+        self.url = ""
+        self.cutchoice = ""
+        self.timestart = ""
+        self.timeend = ""
+        self.output_name = ""
+        self.keepchoice = ""
 
-def download(link):
+    def get_input(self):
+        self.url = input("Enter URL: ")
+        self.cutchoice = self.validate_input("Would you like to trim the video? Y/N: ")
+
+        if self.cutchoice == "Y":
+            self.timestart = int(input("What is the starting point in seconds?: "))
+            self.timeend = int(input("What is the ending point in seconds?: "))
+            self.output_name = input("What would you like the file name to be? (Without file extension): ")
+        elif self.cutchoice == "N":
+            download_video(self.url)
+            sys.exit()
+
+        self.keepchoice = self.validate_input("Would you like to keep the full video? Y/N: ")
+
+    def validate_input(self, prompt):
+        while True:
+            choice = input(prompt)
+            if choice == "Y" or choice == "N":
+                return choice
+            print("Invalid input")
+
+
+def download_video(name):
     try:
-        yt = YouTube(link)
+        user = os.getlogin()
+        yt = YouTube(name)
         yt = yt.streams.get_highest_resolution()
         filepath = yt.download(f"C:\\Users\\{user}\\Videos\\")
         filename = yt.default_filename
@@ -15,42 +46,39 @@ def download(link):
         return filepath
     except RegexMatchError:
         print("Error: Invalid link or video unavailable")
+        sys.exit()
 
-link = input("Enter URL: ")
-filename = download(link)
+user_input = UserInput()
+user_input.get_input()
 
-cutchoice = input("Would you like to cut the video? Y/N: ")
+if user_input.cutchoice == "N":
+    name = download_video(user_input.url)
+    print(f"Video downloaded: {name}")
+    sys.exit(1)
 
-while cutchoice != "Y" and cutchoice != "N":
-    print("Invalid input")
-    cutchoice = input("Would you like to cut the video? Y/N: ")
+name = download_video(user_input.url)
 
-if cutchoice == "N":
-    print("Finished")
+cutchoice = user_input.cutchoice
 
-elif cutchoice == "Y":
-    timestart = int(input("What is the starting point in seconds: "))
-    timeend = int(input("What is the ending point in seconds: "))
-    video = VideoFileClip(filename)
-    clip = video.subclip(timestart, timeend)
-    output_name = input("What would you like the file name to be? (Do not include file extension): ")
-    output_name = output_name + ".mp4"
-    output_path = f"C:\\Users\\{user}\\Videos\\" + output_name
-    clip.write_videofile(output_path)
-    print(f"Trimmed video saved as {output_name}")
-    clip.close()
+user = os.getlogin()
+timestart = user_input.timestart
+timeend = user_input.timeend
+video = VideoFileClip(name)
+clip = video.subclip(timestart, timeend)
+output_name = user_input.output_name + ".mp4"
+output_path = f"C:\\Users\\{user}\\Videos\\" + output_name
+clip.write_videofile(output_path)
+print(f"Trimmed video saved as {output_name}")
+clip.close()
 
-    keepchoice = input("Would you like to keep the full video? Y/N: ")
+keepchoice = user_input.keepchoice
 
-    while keepchoice != "Y" and keepchoice != "N":
-        print("Invalid input")
+if keepchoice == "Y":
+    print("File kept")
+elif keepchoice == "N":
+    os.remove(name)
+    print("File deleted")
 
-    if keepchoice == "N":
-        os.remove(filename)
-        print("File deleted")
-
-    elif keepchoice == "Y":
-        print("File kept")
 
 
 
